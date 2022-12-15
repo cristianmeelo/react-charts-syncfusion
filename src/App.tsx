f8eca590544485973e810446ae83914dde6cab6f
Cristian Melo <cristianmeelo@gmail.com>
2022-12-15 18:34:51 -0300

    feat: :sparkles: implemented BPM chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,56 @@
-import "./App.css"
+import "./App.css";
 
+import {
+  Category,
+  ChartComponent,
+  ColumnSeries,
+  Inject,
+  AreaSeries,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  StripLine,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+
+import { salesData } from "./assets/data";
 export const App = () => {
   return (
-<>
-<h1>
-Hello World
-
-</h1>
-</>
-
-  )
-}
-
+    <>
+      <h1>Ta bombando isso hein</h1>
+      <div>
+        <ChartComponent
+          width="650"
+          height="350"
+          primaryXAxis={{
+            valueType: "Category",
+            title: "Graphic BPM",
+            interval: 5,
+            minimum: 0,
+            maximum: 31,
+          }}
+          primaryYAxis={{
+            interval: 50,
+            minimum: 0,
+            maximum: 299,
+          }}>
+          <Inject
+            services={[ColumnSeries, Tooltip, AreaSeries, Category, StripLine]}
+          />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={salesData}
+              xName="x"
+              yName="y"
+              name="BPM"
+              fill="#4BA7D9"
+              opacity={0.6}
+              type="Area"
+              width={4}
+              border={{ width: 4 }}
+            />
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </>
+  );
+};
